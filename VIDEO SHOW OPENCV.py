82d27f3d0ae2d1e8b0578201bb12bac130283d91
PROGRAMMINGSITE <89089753+PROGRAMMINGSITE@users.noreply.github.com>
2021-08-17 08:01:32 -0700

    VIDEO SHOW IN OPENCV-PYTHON

---
+++ VIDEO SHOW OPENCV.py
@@ -0,0 +1,11 @@
+import cv2
+cap = cv2.VideoCapture(1)
+
+while ( True):
+    ret, frame = cap.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1)  & 0xFF == ord('q'):
+       break
+cv2.imshow('frame',cap)
+cap.release()
+cv2.destroyAllWindow()
