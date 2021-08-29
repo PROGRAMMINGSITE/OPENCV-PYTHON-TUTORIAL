e0865bb15e4f051ae5e314be6df95b85f1041904
ABDUL AHAD RAZA <89089753+PROGRAMMINGSITE@users.noreply.github.com>
2021-08-29 07:40:01 -0700

    Add files via upload

---
+++ Canny Edge Detection.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread("A.png")
+img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+canny = cv2.Canny(img, 100, 200)
+
+titles = ['image', 'canny']
+images = [img, canny]
+for i in range(2):
+    plt.subplot(1, 2, i+1), plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+
+plt.show()
